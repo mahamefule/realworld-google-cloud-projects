f08344676cab1f310b9c0f15ed5f2f2547bd902a
Michael Ahamefule <101369463+mahamefule@users.noreply.github.com>
2023-07-24 18:07:13 -0700

    Update wp-config.php

--- database-config/wp-config.php
+++ database-config/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'admin' );
 
 /** Database hostname */
-define( 'DB_HOST', '34.172.39.195' );
+define( 'DB_HOST', '34.28.222.119' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
